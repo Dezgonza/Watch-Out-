ba244ba78f3c40324e48c2100fb32397af949cb8
MrGonza <38736930+Dezgonza@users.noreply.github.com>
2019-09-14 17:12:21 -0300

    Update color_track.py

--- color_track.py
+++ color_track.py
@@ -1,30 +1,26 @@
-import numpy as np
-from scipy import ndimage
-from math import sqrt
 import cv2, serial, time
+import numpy as np
 
-arduino = serial.Serial('COM7', 9600)
+arduino = serial.Serial('COM5', 9600)
 time.sleep(2)
-cap = cv2.VideoCapture(0)
-margen = 8
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cv2.GaussianBlur(frame, (15,15), 8)
-    x_centr , y_centr = frame.shape[1]//2, frame.shape[0]//2
-    frame1 = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+cam = cv2.VideoCapture(0)
+y, x = 480, 640 #307200
+margen = 40
+
+while True:
+    _, frame = cam.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    #hsv = cv2.cvtColor(frame1, cv2.COLOR_RGB2HSV)
+    verde_bajos = np.array([55,60,60])
+    verde_altos = np.array([120, 255, 255])
+    rojos_bajos = np.array([-15,61,64])
+    rojos_altos = np.array([20, 255, 255])
+    mask = cv2.inRange(hsv, verde_bajos, verde_altos)
 
-    # definir rango de color a aislar
-    lower_hsv = np.array([-15, 130, 100], dtype=np.uint8 )
-    upper_hsv = np.array([20, 255, 255], dtype=np.uint8)
-
-    # color mask
-    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+    #Eliminamos ruido
+    kernel = np.ones((4,4),np.uint8)
+    mask = cv2.morphologyEx(mask,cv2.MORPH_OPEN,kernel)
+    mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,kernel)
 
     # determinar cantidad de pixeles blancos
     mask_area = np.sum(mask, axis=None)/255.0
@@ -32,36 +28,29 @@ while(True):
     # si hay más de 1500 pixeles blancos
     if mask_area >= 1500:
 
-        # obtener centro
-        y, x = ndimage.measurements.center_of_mass(mask)
-        dist_x, dist_y = x-x_centr, y-y_centr
-        #print('derecha') if dist_x > 0 else print('izquierda')
-        #print('abajo') if dist_y > 0 else print('arriba')
-
-        if dist_y > margen:
-            arduino.write(b'1')
-        elif dist_y < -margen:
-            arduino.write(b'0')
-        elif dist_x > margen:
-            arduino.write(b'4')
-        elif dist_x < -margen:
-            arduino.write(b'3')
-
         try:
-            width = int( sqrt(mask_area) )
-            rx = int( x - width/2.0); ry = int( y - width/2.0)
-
+            _, contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            mayor_contorno = max(contours, key = cv2.contourArea)
+            momentos = cv2.moments(mayor_contorno)
+            cx = float(momentos['m10']/momentos['m00'])
+            cy = float(momentos['m01']/momentos['m00'])
+            width = int(np.sqrt(mask_area))
+            rx = int(cx - width/2.0); ry = int(cy - width/2.0)
             cv2.rectangle(frame, (rx, ry), (rx + width, ry + width), (0, 255, 0), 4)
+            dist_x, dist_y = cx-x//2, cy-y//2
+            if dist_x > margen: arduino.write(b'3') #print('derecha')
+            if dist_x < -margen: arduino.write(b'4') #print('izquierda')
+            if dist_y > margen: arduino.write(b'1') #print('abajo')
+            if dist_y < -margen: arduino.write(b'0') #print('arriba')
+            else: print('bkn')
 
         except ValueError:
             pass
-    # Display the resulting frame
+
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
-
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
-cap.release()
+cam.release()
 cv2.destroyAllWindows()
